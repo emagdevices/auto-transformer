2eee4b9dce1f72c77d788d7ce95d131fb7350bd6
yaswanthbandaru <yaswanthbandaru424@gmail.com>
2022-04-16 15:46:10 +0530

    adding tk

--- gui.py
+++ gui.py
@@ -1,8 +1,16 @@
 from tkinter import *
-import math
+import tkinter as tk 
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master=None)
+        self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
+        self.createWidgets()
 
-# class Application(Tk):
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.grid()
 
-#     def __init__(self):
-
+app = Application()
+app.master.title('Sample application')
+app.mainloop()
