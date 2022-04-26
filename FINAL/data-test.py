f6f4ef0276f3c831561945cf18460d421f9792f8
yaswanthbandaru <yaswanthbandaru424@gmail.com>
2022-04-26 12:47:07 +0530

    adding data folder for simpliciyt

---
+++ FINAL/data-test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import data
+
+lamination = data.lamination_data
+print(data.swg_data)
+# print(lamination)
---
+++ FINAL/data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+lamination_data = pd.read_csv('DATA/EI-Laminations.csv')
+swg_data = pd.read_csv('DATA/EMD - Sheet1.csv') # select the swg the data 
+
